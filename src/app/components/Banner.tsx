b838a8a1eb5526f70c6822b5c48e5ffe44995283
nina <nina.kasimova@gmail.com>
2024-02-08 19:10:15 +0000

    redirect home on app name

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
     return (
         <div className="bg-gray-400 sticky top-0 z-[20] p-4 mx-auto flex w-full text-lg border-b border-gray-500">
                 <div className="w-1/6 flex justify-start">
-                    <p className="font-semibold text-2xl text-blue-950">App Name</p>
+                    <Link href="/" className="font-semibold text-2xl text-blue-950">App Name</Link>
                 </div>
                 <div className="w-1/5 flex justify-around text-accent-content/60 hover:text-accent-content/100">
                     <Link href="/books-list">Your books</Link>
