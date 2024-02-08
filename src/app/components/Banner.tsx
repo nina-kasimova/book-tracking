f1998365939559918f91692f261e38d17ee63e94
nina <nina.kasimova@gmail.com>
2024-02-08 19:40:20 +0000

    add about info and useful links

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 const Banner = () => {
     return (
-        <div className="bg-green-900 sticky top-0 z-[20] p-4 mx-auto flex w-full text-lg border-b border-gray-500">
+        <div className="bg-primary sticky top-0 z-[20] p-4 mx-auto flex w-full text-lg border-b border-gray-500">
                 <div className="w-1/6 flex justify-start">
                     <Link href="/" className="font-semibold text-2xl text-blue-950 text-white">App Name</Link>
                 </div>
-                <div className="w-1/5 flex justify-around text-accent-content/60 hover:text-accent-content/100 text-white">
-                    <Link href="/books-list">Your books</Link>
-                    <Link href="/about">About</Link>
+                <div className="w-1/5 flex justify-around">
+                    <Link href="/books-list" className="text-accent-content/60 hover:text-accent-content/100 text-white">Your books</Link>
+                    <Link href="/about" className="text-accent-content/60 hover:text-accent-content/100 text-white">About</Link>
                 </div>
         </div>
     );
